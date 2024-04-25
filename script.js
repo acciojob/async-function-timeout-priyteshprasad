3bf6453978e9adccc0da63766fdc63784ee8b369
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-04-25 12:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn")
+btn.addEventListener('click', myFunction)
+async function myFunction() {
+	let userInput = document.getElementById("text").value;
+	let userDelay = document.getElementById("delay").value;
+	await setTimeout(()=>{
+		document.getElementById("output").innerHTML = userInput;
+	}, userDelay)
+  
+}
